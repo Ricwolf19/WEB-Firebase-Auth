079618e70a3132ec9ae70b5395f839a7727fe91f
Ricwolf19 <rhtc19@gmail.com>
2024-02-07 15:54:01 -0600

    CRUD added to Home

---
+++ src/Components/Crud.jsx
@@ -0,0 +1,101 @@
+import { Button, Input } from "@nextui-org/react"
+import { useEffect, useState } from "react";
+import { db } from "../Firebase";
+import { addDoc, collection, doc, getDocs, deleteDoc, updateDoc } from "firebase/firestore";
+//añadir funcion de firebase para poder agregar un elemento a la base de datos con addDoc
+
+
+export function Crud() {
+    const [fname, setFName] = useState('');
+    const [lname, setLName] = useState('');
+    const [id, setId] = useState('');
+    const [show, setShow] = useState(false)
+    const [val, setVal] = useState([])
+    const value = collection(db, "patients")
+
+    useEffect(() => {
+        const getData = async () => {
+          try {
+            const dbVal = await getDocs(value);
+            setVal(dbVal.docs.map(doc => ({ ...doc.data(), id: doc.id })));
+          } catch (error) {
+            console.error("Error fetching data:", error);
+          }
+        };
+    
+        getData();
+      }, []); // Solo se ejecutará al montar el componente
+
+    //Funcion para crear un usuario con los parametros que elegimos
+    const handleCreate = async () => {
+        await addDoc(value, { firsName: fname, lastName: lname })
+    }
+
+    //Funcion para borrar un usuario con parametro de id
+    const handleDelete = async (id) => {
+        const deleteVal = doc(db, "patients", id)
+        await deleteDoc(deleteVal)
+    }
+
+    //Funcion para poder reactualizar el formulario y con reutilizar el mismo, asi no teniendo que crear otro
+    //Asignando a la vez el id y los nombres a los mismos inputs para corregir y cambiando el boton de crear por update
+    const handleEdit = async (id, firstName, lastName) => {
+        setFName(firstName);
+        setLName(lastName);
+        setId(id);
+        setShow(true);
+    }
+
+    const handleUpdate = async () => {
+        const updateData = doc(db, "patients", id);
+        await updateDoc(updateData, { firstName: fname, lastName: lname });
+        setShow(false);
+    }
+
+    return (
+        <div className="text-center">
+            <Input
+                type="text"
+                color="warning"
+                label="First Name"
+                value={fname}
+                onChange={(e) => setFName(e.target.value)}
+                className="max-w-[220px] pb-2"
+            />
+
+            <Input
+                type="text"
+                color="warning"
+                label="Last name"
+                value={lname}
+                onChange={(e) => setLName(e.target.value)}
+                className="max-w-[220px] pb-5"
+            />
+
+            {!show ? <Button onClick={handleCreate} radius="full" className=" bg-gradient-to-tr from-pink-500 to-yellow-500 text-white shadow-lg">
+                Create Patient
+            </Button>
+                :
+                <Button onClick={handleUpdate} radius="full" className=" bg-gradient-to-tr  from-blue-500 text-white shadow-lg">
+                    Update Patient
+                </Button>}
+
+            <h1 className="pb-5 pt-5 text-2xl text-red-700">The patients are: </h1>
+            {
+                val.map(values =>
+                    <div key={values.id}>
+                        <h1 className="pb-2">{values.firstName}, {values.lastName}</h1>
+                        <Button color="danger" variant="bordered" onClick={() => handleDelete(values.id)}> {/*Si quitas el () se ejecuta automaticamente la instruccion que se tenga al lado, por todo el mapeado*/}
+                            Delete user
+                        </Button>
+                        <Button color="primary" variant="bordered" onClick={() => handleEdit(values.id, values.firstName, values.lastName)}> {/**/}
+                            Edit user
+                        </Button>
+                        <br /><br />
+                    </div>
+                )
+            }
+
+        </div>
+    )
+}
--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,4 +1,5 @@
 import { useAuth } from "../context/authContext" //Se importa el useAuth del authContext para poder usarlo en el home
+import { Crud } from "./Crud" 
 
 export function Home() {
 
@@ -28,31 +29,36 @@ export function Home() {
         <img src={user.photoURL || '/public/user-icon.jpg'} className="h-full w-full text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-200" />
       </div>
       <br />
-      <h1 className="text-4xl font-bold text-gray-800 dark:text-gray-200 mb-4">Welcome, User - {user.displayName || user.email}</h1>
+      <h1 className="text-4xl font-bold text-gray-800 dark:text-gray-200 mb-4">Welcome, {user.displayName || user.email}</h1>
+      
       <button className="mt-4 px-6 py-2 text-white bg-red-500 rounded-full transition-all transform hover:scale-110 hover:bg-red-600" onClick={handleLogout}>
         Logout
       </button>
+      <br />
+
+      <Crud></Crud>
     </div>
   )
 }
 
-function UserIcon(props) {
-  return (
-    <svg
-      {...props}
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    >
-      <path d="M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2" />
-      <circle cx="12" cy="7" r="4" />
-    </svg>
-  )
-}
+
+// function UserIcon(props) {
+//   return (
+//     <svg
+//       {...props}
+//       xmlns="http://www.w3.org/2000/svg"
+//       width="24"
+//       height="24"
+//       viewBox="0 0 24 24"
+//       fill="none"
+//       stroke="currentColor"
+//       strokeWidth="2"
+//       strokeLinecap="round"
+//       strokeLinejoin="round"
+//     >
+//       <path d="M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2" />
+//       <circle cx="12" cy="7" r="4" />
+//     </svg>
+//   )
+// }
 
